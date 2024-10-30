3592cb79b7e484bfd334af766cd0b9ffe558473c
Matthew Chenette <mpchenette@github.com>
2024-10-30 09:24:29 -0700

    added to main.c to see if GHAS catches things in a scan

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include "main.h"
 
+void uninitializedVariableExample() {
+    int x;
+    printf("Value of x: %d\n", x); // Use of uninitialized variable
+}
+
+void nullPointerDereferenceExample() {
+    int *ptr = NULL;
+    *ptr = 42; // Dereferencing a null pointer
+}
+
+void bufferOverflowExample(const char *input) {
+    char buffer[10];
+    strcpy(buffer, input); // Potential buffer overflow if input is larger than 10 characters
+    printf("Buffer content: %s\n", buffer);
+}
+
 int main() {
     printf("Hello, World!\n");
+    
+    // vars.c
+    uninitializedVariableExample();
+
+    // pointer.c
+    nullPointerDereferenceExample();
+
+    // buffer.c
+    bufferOverflowExample("This is a very long input that will overflow the buffer");
+
     return 0;
 }
